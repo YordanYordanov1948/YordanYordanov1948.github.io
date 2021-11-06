cc46b21b63d68d1766e37c71185a167313fa6db4
Yordan Yordanov <46786238+YordanYordanov1948@users.noreply.github.com>
2021-11-06 13:03:17 +0200

    desktop version is ready

---
+++ resources/javascript/script.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+
+window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
